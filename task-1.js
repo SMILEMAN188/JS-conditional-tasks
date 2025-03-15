147d7df61c516e02830daaf2653689c64b249fce
Badhon deb <badhondeb100@gmail.coml>
2025-03-15 22:17:12 +0600

    task-1 done

---
+++ task-1.js
@@ -0,0 +1,8 @@
+let money =400;
+if (money>500){
+    console.log("Bye burger more then 500 tk,thats why you have got a free Coke");
+}
+else{
+    console.log("Add more 30 tk to bye coke");
+    
+}
